5526d21c39792e0a194f95bd6f362a47b453eea6
16austing <85756438+SponegBobo@users.noreply.github.com>
2022-05-13 14:40:56 +0000

    Added Attack buttons, Fight event, correct logic to go back to previous button state

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ window = tk.Tk()
 label = tk.Label(text="Pokemon")
 label.pack()
 
-global button1, button2, button3, button4, buttonState
+global button1, button2, button3, button4, buttonState, lastState
 
 buttonState="Outside"
 frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=5)
@@ -19,8 +19,9 @@ button4 = tk.Button(master=frame, text="Quit", height=5, width=15)
 button4.pack(side=tk.LEFT)
 
 def handle_clickGym(event):
-  global button1, button2, button3, button4, buttonState
+  global button1, button2, button3, button4, buttonState, lastState
   buttonState="GymFight"
+  lastState="GymFight"
   button1["text"]="Fight"
   button2["text"]="Potion"
   button3["text"]="Run"
@@ -28,7 +29,7 @@ def handle_clickGym(event):
   buttonListner()
 
 def handle_clickRun(event):
-  global button1, button2,     button3, button4, buttonState
+  global button1, button2, button3, button4, buttonState
   buttonState="Outside"
   button1["text"]="Gym 1"
   button2["text"]="Wild"
@@ -36,13 +37,30 @@ def handle_clickRun(event):
   button4["text"]="Quit"
   buttonListner()
 def handle_clickWild(event):
-  global button1, button2,     button3, button4, buttonState
+  global button1, button2, button3, button4, buttonState, lastState
   buttonState="WildFight"
+  lastState="WildFight"
   button1["text"]="Fight"
   button2["text"]="Catch"
   button3["text"]="Run"
   button4["text"]=""
   buttonListner()
+def handle_clickFight(event):
+  global button1, button2, button3, button4, buttonState, lastState
+  buttonState="Fight"
+  button1["text"]="Attack A"
+  button2["text"]="Attack B"
+  button3["text"]="Attack C"
+  button4["text"]="Attack D"
+  buttonListner()
+
+def handle_clickAttackA(event):
+  global buttonState, lastState
+  ##Perform attack A
+  if lastState=="GymFight":
+    handle_clickGym("<Button-1>")
+  elif lastState=="WildFight":
+    handle_clickWild("<Button-1>")
 
 def buttonListner():
   if buttonState=="Outside":
@@ -51,9 +69,18 @@ def buttonListner():
     button2.bind("<Button-1>", handle_clickWild)
   if buttonState=="GymFight":
     print(buttonState)
+    button1.bind("<Button-1>", handle_clickFight)
     button3.bind("<Button-1>", handle_clickRun)
   if buttonState=="WildFight":
     print(buttonState)
+    button1.bind("<Button-1>", handle_clickFight)
     button3.bind("<Button-1>", handle_clickRun)
+  if buttonState=="Fight":
+    print(buttonState)
+    button1.bind("<Button-1>",handle_clickAttackA)
+    button2.bind("<Button-1>")
+    button3.bind("<Button-1>")
+    button4.bind("<Button-1>")
+    
 buttonListner()
 window.mainloop()

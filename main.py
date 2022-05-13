7e9fc1013e228d995dd0901962437c599345f36f
Benjamin Bridle <105053706+SnoopGod0420@users.noreply.github.com>
2022-05-13 14:48:32 +0000

    .

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-# sys.path is a list of absolute path strings
-sys.path.append('https://replit.com/@BenjaminBridle1/Game#test.py')
-
-import file
-
-print("RK loves men")
+import os
+os.system('python Gui.py')
+os.system('python login.py')
 
